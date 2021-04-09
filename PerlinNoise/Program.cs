3f3df3ef1f24b95634ebf1f4d0b36c2be19e2e43
1dNDN <idndn@inbox.ru>
2021-04-10 02:16:43 +0300

    cleanup

--- PerlinNoise/Program.cs
+++ PerlinNoise/Program.cs
@@ -10,9 +10,8 @@ namespace PerlinNoise
 	{
 		static void Main(string[] args)
         {
-            Random random = new();
             Image<Rgb24> image = new(1000, 1000);
-            Perlin2D perlin = new Perlin2D();
+            Perlin2D perlin = new();
 
             for(int x = 1; x < image.Width; x++)
             for (int y = 1; y < image.Width; y++)
@@ -20,7 +19,6 @@ namespace PerlinNoise
                 byte color = Convert.ToByte((perlin.Noise((x + 0.5F)/100F, (y + 0.5F)/100F, 20) + 1F) * 127F);
                 image[x, y] = new Rgb24(color, 0, 0);
             }
-
 
             image.Save("image.png");
         }
